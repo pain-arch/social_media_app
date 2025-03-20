8d6d5779f9f1f04d957a30656e48ca4261d4c0ae
pain-arch <himulol123@gmail.com>
2025-03-20 21:54:46 +0600

    Refactor RootLayout to use ClerkAndConvexProvider for improved authentication management

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,16 @@
-import { tokenCache } from '@/cache';
 import Initiallayout from '@/components/Initiallayout';
-import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
-import { Stack } from "expo-router";
+import ClerkAndConvexProvider from '@/providers/ClerkAndConvexProvider';
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
-
-if (!publishableKey) {
-  throw new Error(
-    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
-  )
-}
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
-      <ClerkLoaded>
-        <SafeAreaProvider>
-          <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
-            <Initiallayout />
-          </SafeAreaView>
-        </SafeAreaProvider>
-      </ClerkLoaded>
-    </ClerkProvider>
+    <ClerkAndConvexProvider>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+          <Initiallayout />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ClerkAndConvexProvider>
   );
 }
