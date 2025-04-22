c2efc9f1af96d5d30a75daf2ce50e17587bd6cbe
pain-arch <himulol123@gmail.com>
2025-04-22 18:29:42 +0600

    fix: update notification link to direct to user profile instead of notifications

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -13,7 +13,7 @@ export default function Notification({ notification }: any) {
       <View style={styles.notificationItem}>
         <View style={styles.notificationContent}>
           {/* todo: fix later */}
-          <Link href={`/notifications`} asChild>
+          <Link href={`/user/${notification.sender._id}`} asChild>
             <TouchableOpacity style={styles.avatarContainer}>
               <Image 
                 source={notification.sender.image}
